c80c470f327ec85dcda006694349dbe22f159c21
gabriel <gabriel.akake@gmail.com>
2018-10-22 23:30:44 +0000

    favicon commentaires partenaires

--- js/google.map.config.js
+++ js/google.map.config.js
@@ -6,8 +6,8 @@
 		if ($("#map-canvas").length>0) {
 			var map, myLatlng, myZoom, marker;
 			// Set the coordinates of your location
-			myLatlng = new google.maps.LatLng(41.38791700, 2.16991870);
-			myZoom = 12;
+			myLatlng = new google.maps.LatLng(14.6725693, -17.4387355,17);
+			myZoom = 18;
 			function initialize() {
 				var mapOptions = {
 					zoom: myZoom,
